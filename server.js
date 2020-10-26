858bbfdcaa18e976c321c808e52b7933932a7955
Samuel Estrella <samuelmachado1@gmail.com>
2020-10-26 20:32:48 -0300

    feat(bossabox): adjusting api fields

--- server.js
+++ server.js
@@ -16,4 +16,4 @@ requireDir('./src/models');
 
 app.use("/api", require("./src/routes"));
 
-app.listen(3001);
+app.listen(3000);
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate');
+const autoIncrement = require("mongoose-auto-increment");
 
 const ProductSchema = new mongoose.Schema({
+         
     title: {
+        type: String,
+        required: true,
+    },  
+    link: {
         type: String,
         required: true,
     },
@@ -10,15 +16,16 @@ const ProductSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    url: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+    tags: [],
 
+});
+
+autoIncrement.initialize(mongoose.connection);
+ProductSchema.plugin(autoIncrement.plugin, {
+  model: "post", // collection or table name in which you want to apply auto increment
+  field: "_id", // field of model which you want to auto increment
+  startAt: 1, // start your auto increment value from 1
+  incrementBy: 1, // incremented by 1
 });
 
 ProductSchema.plugin(mongoosePaginate);
