d5c9eb2eb9e90e88f268039df76f416978e32d57
Samuel Estrella <samuelmachado1@gmail.com>
2020-10-26 20:48:54 -0300

    feat(bossabox): Adding TAG filter

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,10 +5,12 @@ const Product = mongoose.model("Product");
 module.exports = {
     async index(req, res) {
         const { page = 1 } = req.query;
-        const products =  await Product.paginate({}, {page, limit: 10});
+        const { tag } = req.query;
+        const products =  await Product.paginate({tags: tag}, {page, limit: 10});
        
        return res.json(products);
     },
+    
 
     async show(req, res) {
         const product = await Product.findById(req.params.id);
